50928bce596f552f727b993034e70f5b0adf0e81
Ronaldo-P <93850597+Ronaldo-P@users.noreply.github.com>
2021-11-15 00:14:04 +0700

    TugasPW
    RonaldoPutra_2024250035

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,33 +1,97 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
+    <body data-bs-spy="scroll" data-bs-target=".navbar" data-bs-offset="50">
+
+<nav class="navbar navbar-expand-sm bg-primary navbar-dark fixed-top">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">
+      <img src="mdp.png" alt="MDP" width="60px">
+      Multi Data Palembang
+    </a>
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" href="#section1">Who am I?</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#section2">Personal Data</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#section3">About Me</a>
+      </li>
     </ul>
   </div>
+</nav>
+
+<div id="section1" class="container-fluid bg-danger text-white" style="padding:100px 30px;">
+  <h1>Ronaldo Putra</h1>
+  <hr>
+  <div class="row">
+    <p></p>
+  <div class="col" ><center>
+    <img src="ronaldo.jfif" class="rounded-circle" alt="Cinque Terre" width="190" height="170"> 
+    </center></div>
+    
+  <div class="col">
+    <li>Perkenalkan Ronaldo Putra dengan NPM 2024250035</li>
+    <li>Seorang Mahasiswa MDP </li>
+    <li>Merupakan anak ke-3 dari 4 bersaudara</li>
+    <li>Bertempat tinggal di Palembang</li>
+  </div>
+  <p></p>
+  <p></p>
+  <p></p>
+  <p>    *Hai, nama saya Ronaldo Putra biasa dipanggil Aldo atau Dodo. Saya merupakan seorang mahasiswa MDP yang mengambil jurusan TI/IF dengan NPM 2024250035, alasan saya sendiri memilih jurusan TI dikarenakan saya tertarik dengan ilmu komputer dan berharap untuk kedepannya bisa menggunakan ilmu komputer dengan baik sehingga mendapat pekerjaan yang diinginkan.
+  </p>
+</div>
+</div>
+
+<div id="section2" class="container-fluid bg-warning" style="padding:100px 30px;">
+  <h1>Data Diri</h1>
+  <hr>
+  <div class="row">
+    
+    <div class="col">
+      <h5><u>Biodata</u></h5>
+      <li>Lahir di Palembang, 2 Juli 2002</li>
+      <li>Anak ke-3 dari 4 bersaudara</li>
+      <li>Tinggal di Jl. Slamet Riady No 255</li>
+      <li>Beragama Buddha</li>
+      <li>Jenis Kelamin Laki-laki</li>
+      <li>Merupakan Mahasiswa MDP</li>
+      <li>Bekerja di Kedai B217an</li>
+    </div>
+
+    <div class="col">
+        <h5><u>Pendidikan</u></h5>
+        <li>SD SUMSEL JAYA</li>
+        <li>SMP METHODIST 2</li>
+        <li>SMK METHODIST 2</li>
+        <li>UNIVERSITAS MDP</li>
+    </div>
+
+    <div class="col">
+      <h5><u>Sosial Media</u></h5>
+      <li><img src="ig.png" width="20px"> @ronal2pz</li>
+      <li><img src="fb.png" width="20px"> Ronaldo Putra</li>
+    </div>
+  </div>
+  </div>
+
+<div id="section3" class="container-fluid bg-secondary text-white" style="padding:70px 30px;">
+  <h1>Kesimpulan</h1>
+  <hr>
+  <p></p>
+  <p></p>
+  <p>
+      *Hai,Salam kenal!
+    Saya adalah Ronaldo Putra dengan NPM 2024250035, anak ke-3 dari 4 bersaudara yang tinggal di Lorong Kemas 1 (Kuto),Hobi saya sendiri adalah menonton film movie/series dan bermain game. Saya bekerja di Kedai B217an yang berada di Bangau sebagai Waitress pada shift pagi dan pulang siang sehingga bisa langsung melanjutkan kuliah pada malamnya. jadi, , kegiatan saya bukan hanya seorang pelajar namun juga seorang pekerja.
+    Terima kasih.
+  </p>
+  </div>
+
+</body>
+  </div>
+  
 </template>
 
 <script>
@@ -39,20 +103,3 @@ export default {
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
